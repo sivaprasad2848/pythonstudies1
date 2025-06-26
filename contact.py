58a1e2f58bd16f4bd74bdd607a762282a6619732
sivaprasad2848 <sivaprasad@cybersquare.org>
2025-06-26 14:04:51 +0530

    If Condition Added

--- contact.py
+++ contact.py
@@ -1,12 +1,19 @@
 y=0
 contacts=[]
 while(y==0):
-    name=input("Enter Name")
-    email=input("Enter Email")
-    mobile=input("Enter Mobile")
-    contacts.append((name,email,mobile))
-    #print(contacts)
-    for item in contacts:
-        print(item[0]+" "+item[1]+" "+item[2])
-    
+    print("Menu Program")
+    print("1->For Create New Contact")
+    print("2->For Display Contact")
+    print("3->For Delete Contact")
+    print("4->For Update Contact")
+    opt=int(input("Enter an option"))
+    if(opt==1):
+        name=input("Enter Name")
+        email=input("Enter Email")
+        mobile=input("Enter Mobile")
+        contacts.append((name,email,mobile))
+    if(opt==2):    
+        for item in contacts:
+            print(item[0]+" "+item[1]+" "+item[2])
+        
     y=int(input("Do you want to continue?press 0 for yes"))
