7f5f097a271e32861155c3ad93fa936095dd2161
sivaprasad2848 <sivaprasad@cybersquare.org>
2025-06-26 13:58:33 +0530

    Contact Program Updated

--- contact.py
+++ contact.py
@@ -1,4 +1,12 @@
-name=input("Enter Name")
-email=input("Enter Email")
-mobile=input("Enter Mobile")
-print(name+" "+email+" "+mobile)
+y=0
+contacts=[]
+while(y==0):
+    name=input("Enter Name")
+    email=input("Enter Email")
+    mobile=input("Enter Mobile")
+    contacts.append((name,email,mobile))
+    #print(contacts)
+    for item in contacts:
+        print(item[0]+" "+item[1]+" "+item[2])
+    
+    y=int(input("Do you want to continue?press 0 for yes"))
